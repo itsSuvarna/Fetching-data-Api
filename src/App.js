7d1fab82aba9ffdf754ed10075b56664ed07001b
itsSuvarna <117782206+itsSuvarna@users.noreply.github.com>
2023-04-08 04:19:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@
 //  to take multiple data use multiple setvalue -->setDatas,setMoves.etc
 
 
-
+//method 1
 import React, { useEffect, useState } from "react"
 import axios from "axios"
 
@@ -68,5 +68,69 @@ const[moves,setMoves]=useState()
 export default App;
 
 
+
+
+
+
+
+//method 2&3 
+1) using promises ---> .then & catch
+2) using Async await (try & catch)
+
+//https://codesandbox.io/s/stoic-wave-cgptki?file=/src/App.js
+
+//axios using promises
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [datas, setDatas] = useState([]);
+  //after getting data check in which form by using console
+  //if it in arrays,then to store this data, use array inside useState
+  const [isError, setIsError] = useState("");
+
+  /*using promises ---> .then & catch
+   useEffect(()=>{
+   axios
+   .get("https://jsonplaceholder.typicode.com/posts")
+   .then((res)=>setDatas(res.data))
+   //res.data is in form of array ,
+   //then to store this ddata, use array inside useState
+    .catch((error)=>setIsError(error.message))
+
+   },[]);
+*/
+
+  /* using Async await (try & catch) */
+
+  //make arrow  or simple function with async
+  const getData = async () => {
+    try {
+      const res = await axios.get("https://jsonpaceholder.typicode.com/posts");
+      setDatas(res.data);
+    } catch (error) {
+      setIsError(error.message);
+      }
+    };
+useEffect(() => { getData(); }, []);
+
+  return (
+    <>
+      {isError !== "" && <h2>{isError}</h2>}
+      {datas.slice(0, 12).map((post) => {
+        //take first 12 post from array
+        const { id, title, body } = post;
+        return (
+          <div key={id}>
+            <h1>{title}</h1>
+            <p>{body}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+export default App;
 
 
